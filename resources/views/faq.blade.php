c99abe80836ed4e13001dd0137dbec5f34df946e
wutlu <mutlu@etsetra.com>
2021-12-03 21:45:02 +0300

    .

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -13,6 +13,16 @@
     }
 @endpush
 
+@push('css')
+	pre {
+	    white-space: pre-wrap;       /* Since CSS 2.1 */
+	    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+	    white-space: -pre-wrap;      /* Opera 4-6 */
+	    white-space: -o-pre-wrap;    /* Opera 7 */
+	    word-wrap: break-word;       /* Internet Explorer 5.5+ */
+	}
+@endpush
+
 @section('content')
 	@component('includes.header')
 		@slot('title', 'F.A.Q.')
