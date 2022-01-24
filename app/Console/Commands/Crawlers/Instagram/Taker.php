6f477d522e706d6be7bf836f6c7809e50abe24ed
unknown <mutlu@etsetra.com>
2022-01-24 19:14:44 +0300

    .

--- app/Console/Commands/Crawlers/Instagram/Taker.php
+++ app/Console/Commands/Crawlers/Instagram/Taker.php
@@ -58,7 +58,7 @@ class Taker extends Command
             ->where('source', 'instagram')
             ->where('type', 'hashtag')
             ->where('request_at', '<=', DB::raw("NOW() - INTERVAL '1 minutes' * request_frequency"))
-            ->orderBy('request_at', 'asc')
+            ->orderBy('id', 'asc')
             ->get();
 
         if (count($tracks))
